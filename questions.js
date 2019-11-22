9b46213d92e9bceb6651133f6537c07484ea026f
ananya92 <1992ananya@gmail.com>
2019-11-22 11:57:32 +1030

    Added the quiz questions object in questions.js and linked the file index.html

---
+++ questions.js
@@ -0,0 +1,160 @@
+var htmlQuestions = [
+    {
+      title: "What does HTML stand for?",
+      choices: ["Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hyper Text Markup Language", "Hyper Tool Markup Language"],
+      answer: "Hyper Text Markup Language"
+    },
+    {
+      title: "Who makes the web standards?",
+      choices: ["Google", "The World Wide Web Consortium", "Mozilla", "Microsoft"],
+      answer: "The World Wide Web Consortium"
+    },
+    {
+        title: "What is the correct HTML element to define important text?",
+        choices: ["<important>", "<i>", "<strong>", "<b>"],
+        answer: "<strong>"
+    },
+    {
+        title: "How can you make a numbered list?",
+        choices: ["<ol>", "<dl>", "<list>", "<ul>"],
+        answer: "<ol>"
+    },
+    {
+        title: "Which is the correct HTML element for playing video files?",
+        choices: ["<media>", "<movie>", "<iframe>", "<video>"],
+        answer: "<video>"
+    },
+    {
+        title: "In HTML, onblur and onfocus are:",
+        choices: ["Style attributes", "Event attributes", "HTML elements", "Global variables"],
+        answer: "Event attributes"
+    },
+    {
+        title: "The HTML <canvas> element is used to:",
+        choices: ["create draggable elements", "manipulate data in MySQL", "display database records", "draw graphics"],
+        answer: "draw graphics"
+    },
+    {
+        title: "Which input type defines a slider control?",
+        choices: ["slider", "controls", "range", "search"],
+        answer: "range"
+    },
+    {
+        title: "In HTML, which attribute is used to specify that an input field must be filled out?",
+        choices: ["placeholder", "validate", "formvalidate", "required"],
+        answer: "required"
+    },
+    {
+        title: "Which HTML element defines navigation links?",
+        choices: ["nav", "navbar", "navigate", "navigation"],
+        answer: "nav"
+    },
+];
+
+var cssQuestions = [
+    {
+      title: "What does CSS stand for?",
+      choices: ["Creative Style Sheets", "Colorful Style Sheets", "Computer Style Sheets", "Cascading Style Sheets"],
+      answer: "Cascading Style Sheets"
+    },
+    {
+      title: "What is the correct place to link an external stylesheet in HTML document?",
+      choices: ["Before the <head>", "In the <head>", "Beginning of <body>", "At end of the document"],
+      answer: "In the <head>"
+    },
+    {
+      title: "Which HTML tag is used to define an internal style sheet?",
+      choices: ["<style>", "<css>", "<script>", "<intern>"],
+      answer: "<style>"
+    },
+    {
+      title: "Which HTML attribute is used to define inline styles?",
+      choices: ["styles", "class", "font", "style"],
+      answer: "style"
+    },
+    {
+      title: "How do you insert a comment in a CSS file?",
+      choices: ["'this is a comment", "// this is a comment", "/* this is a comment */", "<!-- this is a comment -->"],
+      answer: "/* this is a comment */"
+    },
+    {
+      title: "Which property is used to change the background color?",
+      choices: ["bgcolor", "background-color", "color", "bg-color"],
+      answer: "background-color"
+    }, 
+    {
+      title: "Which property is used to change the text color?",
+      choices: ["text-color", "font-color", "color", "fgcolor"],
+      answer: "color"
+    },
+    {
+      title: "Which property is used to change the text size?",
+      choices: ["text-size", "font-size", "text-style", "font-style"],
+      answer: "font-size"
+    },      
+    {
+      title: "How do you make the text bold?",
+      choices: ["font-style:bold;", "font-size:bold;", "text-style:bold;", "font-weight:bold;"],
+      answer: "font-weight:bold;"
+    },
+    {
+      title: "What is the default value of position property?",
+      choices: ["static", "fixed", "relative", "absolute"],
+      answer: "static"
+    }, 
+];
+
+var javascriptQuestions = [
+    {
+      title: "Inside which element do we put the Javascript",
+      choices: ["<script>", "<javascript>", "<js>", "<scripting>"],
+      answer: "<script>"
+    },
+    {
+      title: "Where is the correct place to insert a JavaScript?",
+      choices: ["<body>", "either <head> or <body>", "<head>", "at end of document"],
+      answer: "either <head> or <body>"
+    },
+    {
+      title: "What is the attribute for referring to an external script?",
+      choices: ["script-name", "name", "href", "src"],
+      answer: "src"
+    },
+    {
+      title: "How do you insert a comment in Javascript?",
+      choices: ["'this is a comment", "// this is a comment", "*/ this is a comment", "<!-- this is a comment -->"],
+      answer: "// this is a comment"
+    },
+    {
+      title: "What is the correct way to write a Javascript array?",
+      choices: ["var colors = {\"red\", \"green\", \"blue\"}", "var colors = [\"red\", \"green\", \"blue\"]", "var colors = \"red\", \"green\", \"blue\"", "var colors = (\"red\", \"green\", \"blue\")"],
+      answer: "var colors = [\"red\", \"green\", \"blue\"]"
+    },
+    {
+      title: "How do you detect the client's browser name?",
+      choices: ["client.navName", "browser.appName", "navigator.appName", "window.name"],
+      answer: "navigator.appName"
+    },
+    {
+      title: "Which event occurs when user clicks on an HTML element?",
+      choices: ["onclick", "onmouseclick", "onchange", "onmouseover"],
+      answer: "onclick"
+    },
+    {
+      title: "What is the keyword to declare a Javascript variable?",
+      choices: ["variable", "v", "var", "jvar"],
+      answer: "var"
+    },
+    {
+      title: "What will be the output of the following code: document.write(typeof(24.49));",
+      choices: ["float", "number", "integer", "double"],
+      answer: "number"
+    },
+    {
+      title: "What method in JavaScript is used to remove the whitespace at the beginning and end of any string",
+      choices: ["strip()", "trim()", "alter()", "clip()"],
+      answer: "trim()"
+    },
+  ];
+
+  
